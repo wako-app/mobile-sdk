e4445e335d4532809649370acd62d774061087a0
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2019-08-28 14:47:09 +0200

    Add subtitleIndex

--- src/services/kodi/forms/player/kodi-player-set-subtitle.form.ts
+++ src/services/kodi/forms/player/kodi-player-set-subtitle.form.ts
@@ -1,10 +1,14 @@
 import { KodiApiService } from '../../services/kodi-api.service';
 
 export class KodiPlayerSetSubtitleForm {
-  static submit(playerId: number, enabled: boolean) {
+  static submit(playerId: number, enabled: boolean, subtitleIndex?: number) {
     return KodiApiService.doAction('Player.SetSubtitle', {
       playerid: playerId,
-      subtitle: enabled ? 'on' : 'off',
+      subtitle: enabled
+        ? Number.isInteger(subtitleIndex)
+          ? subtitleIndex
+          : 'on'
+        : 'off',
       enable: enabled
     });
   }
