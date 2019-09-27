3c85ed3ad85a6219043f09a2f24a23756371a269
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2019-09-27 16:30:18 +0200

    remove unused argument

--- src/services/kodi/forms/player/kodi-player-set-audio-stream.form.ts
+++ src/services/kodi/forms/player/kodi-player-set-audio-stream.form.ts
@@ -1,7 +1,7 @@
 import { KodiApiService } from '../../services/kodi-api.service';
 
 export class KodiPlayerSetAudioStreamForm {
-  static submit(playerId: number, enabled: boolean, index: number) {
+  static submit(playerId: number, index: number) {
     return KodiApiService.doAction('Player.SetAudioStream', {
       playerid: playerId,
       stream: index
