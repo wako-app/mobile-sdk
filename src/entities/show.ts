821c121af3aac48af7b2c33f557797b74760483f
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2020-07-14 22:15:10 +0200

    v3.2.5 - New show status

--- src/entities/show.ts
+++ src/entities/show.ts
@@ -1,7 +1,7 @@
 import { Season } from './season';
 import { BaseMedia } from './base-media';
 
-export declare type ShowStatus = 'ended' | 'canceled' | 'returning series';
+export declare type ShowStatus = 'ended' | 'canceled' | 'returning series' | 'tba' | 'airing';
 export declare type ShowAnimeType = 'tv' | 'special' | 'ova' | 'movie' | 'music video' | 'ona';
 
 export interface Show extends BaseMedia {
